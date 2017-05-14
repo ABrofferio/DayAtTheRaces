9092c7527a34ce59b402d9a3882c9c868c7dc678
Aja Brofferio <aja.brofferio@gmail.com>
2017-05-13 20:49:46 -0700

    declaration of winner

--- race.js
+++ race.js
@@ -1,8 +1,7 @@
-let mycar1 = document.getElementById("car1"); 
-let mycar2 = document.getElementById("car2"); 
-let mycar3 = document.getElementById("car3"); 
-let mycar4 = document.getElementById("car4"); 
-let myflag = document.getElementById("flag");
+const mycar1 = document.getElementById("car1"); 
+const mycar2 = document.getElementById("car2"); 
+const mycar3 = document.getElementById("car3"); 
+const mycar4 = document.getElementById("car4"); 
 
 function myMove(elem, pos, speed) {
     let id = setInterval(frame, speed);
@@ -17,10 +16,53 @@ function myMove(elem, pos, speed) {
 }
 
 $('body').on('keydown', function(){
-    //$('#flag').animate({ left: "+=500px" }, 100);
-    myMove(myflag,0,10);
-    myMove(mycar1,0,5);
-    myMove(mycar2,0,4);
-    myMove(mycar3,0,2);
-    myMove(mycar4,0,10);
+    $('img').attr("title","begin");
+    let speed1 = Math.floor((Math.random() * 10) + 5);
+    let speed2 = Math.floor((Math.random() * 10) + 5);
+    let speed3 = Math.floor((Math.random() * 10) + 5);
+    let speed4 = Math.floor((Math.random() * 10) + 5);
+    myMove(mycar1,0,speed1);
+    myMove(mycar2,0,speed2);
+    myMove(mycar3,0,speed3);
+    myMove(mycar4,0,speed4);
+    let winner = Math.min(speed1, speed2, speed3, speed4);
+    if (winner === speed1){
+        $('#winner').delay(2000)
+        .queue(()=> {
+            $('#winner').html("Car One is the winner!");
+            $('#winner').attr("title","win");
+        });
+    }
+    else if (winner === speed2){
+        $('#winner').delay(2000)
+        .queue(()=> {
+            $('#winner').html("Car Two is the winner!");
+            $('#winner').attr("title","win");
+        });
+    }
+    else if (winner === speed3){
+        $('#winner').delay(2000)
+        .queue(()=> {
+            $('#winner').html("Car Three is the winner!");
+            $('#winner').attr("title","win");
+        });
+    }
+    else if (winner === speed4){
+        $('#winner').delay(2000)
+        .queue(()=> {
+            $('#winner').html("Car Four is the winner!");
+            $('#winner').attr("title","win");
+        });
+    }
 });
+
+$('button').on('click', function(){
+    $('img').removeAttr("title");
+    $('#winner').removeAttr("title");
+    mycar1.style.left = 0+"%";
+    mycar2.style.left = 0+"%";
+    mycar3.style.left = 0+"%";
+    mycar4.style.left = 0+"%";
+});
+    
+
